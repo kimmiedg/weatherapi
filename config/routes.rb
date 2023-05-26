d40c34dd228dae7c8b0f72c784c57a398994ad64
kim <kdegracia.dnc@gmail.com>
2023-05-26 22:04:09 +0800

    Added configuration for OpenTelemetry

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :v1 do
+    resources :traces, only: [:create]
     get 'weathers/forecast', to: 'weathers#forecast'
   end
   match '*a', to: 'errors#not_found', via: :get
